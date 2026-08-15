a4e2b4c9ec861ab1c85ba9c747be80f8daef664c
tphatwebdev <trantienphat13112004@gmail.com>
2026-08-15 09:03:56 +0000

    create APIs with Express Router

--- src/server.js
+++ src/server.js
@@ -2,9 +2,12 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
   const app = express()
+
+  app.use('/v1', APIs_V1)
 
   app.get('/', (req, res) => {
     res.end('<h1>Hello World!</h1><hr>')

1a643038aa8900ecf88c30550989c148bb920631
tphatwebdev <trantienphat13112004@gmail.com>
2026-08-15 08:02:16 +0000

    connect mongoDB

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,29 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
+
+let trelloDatabaseInstance = null
+
+// khởi tạo 1 đối tượng mongoClientInstance để connect tới mongoDB
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// kết nối tới Database
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+// func GET_DB (khong async) export ra trello db instance sau khi connect thành công
+// để sử dụng ở nhiều nơi trong code
+// chỉ luôn gọi GET_DB khi connect thành công
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Mus connect to db first!')
+  return trelloDatabaseInstance
+}

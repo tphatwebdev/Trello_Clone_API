25bf913a4ba9b587697ca6bc3246a74a441a510c
tphatwebdev <trantienphat13112004@gmail.com>
2026-08-17 06:52:26 +0000

    config cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import cors from 'cors'
+import { corsOptions } from './config/cors'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
@@ -7,7 +9,7 @@ import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
-
+  app.use(cors(corsOptions))
   // enable req.body json data
   app.use(express.json())
 
